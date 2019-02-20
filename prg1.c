0859537e3920ec739aba526360c185a1b8c61efd
shilpA213 <43333451+shilpA213@users.noreply.github.com>
2019-02-20 14:23:16 +0530

    Update and rename README.md to prg1.c

---
+++ prg1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+int num;
+print f("enter the number");
+scanf("%d",&num);
+if (num>0)
+printf("%d is a positive number",num);
+elseif(num<0)
+printf("%d is a negative number",num);
+elseif(num==0)
+printf("%d is zero",num);
+return0;
+}
